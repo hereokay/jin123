c33d8745e09541d044cd75c12976690ef1282fc3
SeungBeomHu <hsb0423@naver.com>
2023-12-22 14:57:28 +0900

    feat : user Mapper 추가

---
+++ src/main/java/com/muselive/bemuselive/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.muselive.bemuselive.mapper;
+
+import com.muselive.bemuselive.VO.User;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.Map;
+
+@Mapper
+public interface UserMapper {
+
+    User getUserInfo(Map map);
+
+}
