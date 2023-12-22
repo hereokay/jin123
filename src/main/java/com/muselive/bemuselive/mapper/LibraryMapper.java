71daf6618ac1818f8cc80fa7776e513da48eceba
yookyung Choi <ykc131719@gmail.com>
2023-12-22 15:04:19 +0900

    feat : library mapper 생성, VO 생성

--- src/main/java/com/muselive/bemuselive/mapper/LibraryMapper.java
+++ src/main/java/com/muselive/bemuselive/mapper/LibraryMapper.java
@@ -9,5 +9,5 @@ import org.springframework.stereotype.Component;
 @Mapper
 public interface LibraryMapper {
     List<Map> getLibrary();
-    Map getUserBySchoolId(Integer school_id);
+    Map getLibraryTotalFee(Integer school_id);
 }
