ab7b394d42d202e1f345d14e68bfc124c9a395fe
Chris <hereokay88@gmail.com>
2023-12-22 17:08:13 +0900

    feat: payment controller 구현

--- src/main/java/com/muselive/bemuselive/controller/PaymentCTRL.java
+++ src/main/java/com/muselive/bemuselive/controller/PaymentCTRL.java
@@ -1,5 +1,6 @@
 package com.muselive.bemuselive.controller;
 
+import com.muselive.bemuselive.service.EthereumService;
 import com.muselive.bemuselive.service.PayService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.web3j.protocol.core.methods.response.TransactionReceipt;
 
+import java.util.HashMap;
 import java.util.Map;
 
 
@@ -18,21 +21,39 @@ public class PaymentCTRL {
     @Autowired
     PayService payService;
 
+    @Autowired
+    EthereumService ethereumService;
+
+
     @PostMapping(value = "/payment")
-    ResponseEntity<?> test(@RequestBody Map body){
+    ResponseEntity<?> test(@RequestBody Map body) throws Exception {
 
 
 
         if(!body.containsKey("school_id")
                 || !body.containsKey("service_id")
                 || !body.containsKey("payment_amount")){
-            return ResponseEntity.badRequest().build();
+            return ResponseEntity.badRequest().body(
+                    "wrong param"
+            );
         }
 
+        // Blockchain
+        TransactionReceipt receipt = ethereumService.reqPay(
+                (int) body.get("school_id"),
+                (int) body.get("service_id"),
+                (int) body.get("payment_amount")
+        );
+
+        if (!receipt.isStatusOK()){
+            return ResponseEntity.badRequest().body("transaction fail");
+        }
+
+        // DB
         Map paymentSuccess =  payService.GeneralPayment(body);
+        paymentSuccess.put("txHash",receipt.getTransactionHash());
 
         log.info("paymentSuccess : {}",paymentSuccess);
-
         return ResponseEntity.ok().body(paymentSuccess);
 
 
